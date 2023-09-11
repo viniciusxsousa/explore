ac652756a341cf794d5938453f3512181355ff08
Vinicius Sousa <viniciusxsousa@outlook.com>
2023-09-11 13:33:50 -0300

    adicionando o exemplo da aula 02

---
+++ js/aula02.js
@@ -0,0 +1,10 @@
+/* 
+    Solicite dois números, faça a soma deles e apresente o resultado final ao usuário.
+*/
+
+let number1 = Number(prompt('Digite o primeiro número:'));
+let number2 = Number(prompt('Digite o segundo número:'));
+
+let sum = number1 + number2;
+
+alert(`A soma de ${number1} mais ${number2} é ${sum}`);
