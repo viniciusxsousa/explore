0c1cd492582a1bdd343df3938d8a18237f05fb1f
Vinicius Sousa <viniciusxsousa@outlook.com>
2023-09-11 10:34:24 -0300

    adicionando exercício da aula 01 de lógica

---
+++ js/aula01.js
@@ -0,0 +1,10 @@
+/*
+    Encontre a solução do problema:
+
+    Pergunte o nome do usuário e escreva a mensagem:
+    "Olá, [nome do usuário]";
+*/
+
+let nomeUsuario = prompt('Bem-vindo, digite seu nome para continuar:');
+
+alert(`Olá, ${nomeUsuario}`);
