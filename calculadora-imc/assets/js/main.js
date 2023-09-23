7e6638d3566e13ba34083c144a6709e0c6c56bd3
Vinicius Sousa <viniciusxsousa@outlook.com>
2023-09-23 15:04:38 -0300

    concluindo a lógica do modal

--- calculadora-imc/assets/js/main.js
+++ calculadora-imc/assets/js/main.js
@@ -1,18 +1,32 @@
 const $formButton = document.querySelector('.main__form button');
 const $inputWeight = document.querySelector('#peso');
 const $inputHeight = document.querySelector('#altura');
+const $modal = document.querySelector('.conteiner__modal');
+const $txtModal = document.querySelector('.card__modal p');
+const $btnClose = document.querySelector('.card__btn');
 
+console.log($txtModal);
+
+
+/* Lista de eventos */
 $formButton.addEventListener('click', (e) => {
     e.preventDefault();
    
     const weight = Number($inputWeight.value);
     const height = Number($inputHeight.value);
 
-    console.log(calcIMC(weight, height));
+    $modal.classList.add('open');
+    $txtModal.innerText = `Seu IMC é de ${calcIMC(weight, height)}`;
 })
 
+$btnClose.addEventListener('click', () => {
+    $modal.classList.remove('open');
+})
+
+/* Funções */
+
 function calcIMC(weight, height) {
-    let imc = weight / (height ** 2);
+    let imc = weight / ((height / 100 )** 2);
     imc = imc.toFixed(2);
     return imc
 }
