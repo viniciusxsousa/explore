95a95ec27f4f8fc459fdc694d6a9f588bbdbf5c5
Vinicius Sousa <viniciusxsousa@outlook.com>
2023-10-06 11:56:25 -0300

    criando a função createRow e fazendo a adição de uma linha na tabela

--- github-favorites/assets/js/favorites.js
+++ github-favorites/assets/js/favorites.js
@@ -1,6 +1,24 @@
 export class Favorites {
     constructor(root) {
         this.root = document.querySelector(root);
+        this.load();
+    }
+
+    load() {
+        this.users = [
+            {
+                login: 'viniciusxsousa',
+                name: 'Vinicius Sousa',
+                public_repos: '20',
+                followers: '10'
+            },
+            {
+                login: 'rafaballerini',
+                name: 'Vinicius Sousa',
+                public_repos: '49',
+                followers: '46.9k'
+            }
+        ]
     }
 }
 
@@ -8,19 +26,47 @@ export class FavoritesView extends Favorites {
     constructor(root) {
         super(root);
 
+        this.tbody = this.root.querySelector('table tbody');
+
         this.update();        
     }
 
     update() {
         this.removeAllTr();
+
+        this.users.forEach( (user) => {
+            const row = this.createRow();
+
+            row.querySelector('.table__user img').src = `https://github.com/${user.login}.png`;
+            row.querySelector('.table__user img').alt = `Foto do perfil de ${user.name}`;
+            row.querySelector('.table__user a p').innerText = user.name;
+            row.querySelector('.table__user a span').innerText = user.login;
+            row.querySelector('.table__repository').innerText = user.public_repos;
+            row.querySelector('.table__followers').innerHTML = user.followers;
+
+            this.tbody.append(row);
+        } )
+
+    }
+
+    createRow() {
+        const tr = document.createElement('tr');
+
+        tr.innerHTML = ` 
+            <td class="table__user">
+            <img src="https://github.com/viniciusxsousa.png" alt="Foto de perfil do usuário">
+            <a href="#"><p>Vinicius Sousa</p><span>viniciusxsousa</span></a>
+            </td>
+            <td class="table__repository">23</td>
+            <td class="table__followers">10</td>
+            <td class="table__action"><button>x</button></td>
+        `;
+
+        return tr
     }
 
     removeAllTr() {
-        const tbody = this.root.querySelector('table tbody');
-
-        console.log(tbody);
-        
-        tbody.querySelectorAll('tr').forEach((row) => {
+        this.tbody.querySelectorAll('tr').forEach((row) => {
             row.remove();
         })
     }
