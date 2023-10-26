791f582b0ac36f314b9a772cd7b2d7de89627c66
Vinicius Sousa <viniciusxsousa@outlook.com>
2023-10-26 10:11:44 -0300

    criando um rota com query params

--- API/src/server.js
+++ API/src/server.js
@@ -10,6 +10,12 @@ app.get('/user/:id', (req, res) => {
     res.send(`O id do usuário é ${req.params.id}`);
 })
 
+app.get('/product', (req, res) => {
+    const {page, limit} = req.query;
+
+    res.send(`Página ${page}, itens por página ${limit}`);
+})
+
 const PORT = 3333;
 
 app.listen(PORT, () => console.log(`Rodando a aplicação na porta ${PORT}`));
