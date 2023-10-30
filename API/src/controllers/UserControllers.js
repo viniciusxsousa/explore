b5145708d3c062ab466a8888e99d8d37fb83e0a4
Vinicius Sousa <viniciusxsousa@outlook.com>
2023-10-30 13:29:10 -0300

    configurando o express-async-errors

--- API/src/controllers/UserControllers.js
+++ API/src/controllers/UserControllers.js
@@ -1,6 +1,12 @@
+const AppError = require('../utils/AppError');
+
 class UserControllers {
     create(req, res) {
         const { name, email, password } = req.body
+
+        if(!name) {
+            throw new AppError('O nome é obrigatório!');
+        }
 
         res.send({name, email, password});
     }
--- API/src/server.js
+++ API/src/server.js
@@ -1,10 +1,30 @@
+require('express-async-errors');
+
 const express = require('express');
+
+const AppError = require('./utils/AppError');
 
 const app = express();
 const routes = require('./routes');
 
 app.use(express.json());
 app.use(routes);
+
+app.use((error, req, res, next) => {
+    if(error instanceof AppError) {
+        return res.status(error.statusCode).json({
+            status: "error",
+            message: error.message
+        })
+    }
+
+    console.error(error);
+
+    return res.status(500).json({
+        status: 'error',
+        message: 'Erro interno'
+    })
+})
 
 const PORT = 3333;
 
