cf75dd1a9102c6a74879025b2b8d10b478f340a9
Vinicius Sousa <viniciusxsousa@outlook.com>
2023-10-30 17:01:37 -0300

    criando a conexão com o bd

--- API/src/server.js
+++ API/src/server.js
@@ -1,5 +1,5 @@
 require('express-async-errors');
-
+const database = require('./database/sqlite');
 const express = require('express');
 
 const AppError = require('./utils/AppError');
@@ -9,6 +9,8 @@ const routes = require('./routes');
 
 app.use(express.json());
 app.use(routes);
+
+database();
 
 app.use((error, request, response, next) => {
     if(error instanceof AppError) {
