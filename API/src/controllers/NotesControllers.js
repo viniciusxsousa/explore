9b4779ff3fc4b2625af7bc96f0f5389566d42778
Vinicius Sousa <viniciusxsousa@outlook.com>
2023-11-05 16:02:01 -0300

    criando a rota para exibir uma nota

--- API/src/controllers/NotesControllers.js
+++ API/src/controllers/NotesControllers.js
@@ -32,6 +32,20 @@ class NotesControllers {
 
         res.json();
     }
+
+    async show(req, res) {
+        const { id } = req.params;
+
+        const note = await knex('notes').where({id}).first();
+        const tags = await knex('tags').where({note_id: id}).orderBy('name');
+        const links = await knex('links').where({note_id: id}).orderBy('created_at');
+
+        return res.json({
+            ...note,
+            tags,
+            links
+        });
+    }
 }
 
 module.exports = NotesControllers;
--- API/src/routes/note.routes.js
+++ API/src/routes/note.routes.js
@@ -7,5 +7,6 @@ const NotesControllers = require("../controllers/NotesControllers");
 const notesControllers = new NotesControllers();
 
 notesRoutes.post('/:user_id', notesControllers.create);
+notesRoutes.get('/:id', notesControllers.show);
 
 module.exports = notesRoutes;
