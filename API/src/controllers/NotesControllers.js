1dae4c30aca9e0e4b19e4628720efb3bd23be84c
Vinicius Sousa <viniciusxsousa@outlook.com>
2023-11-05 17:07:04 -0300

    iplementando o filtro por tags

--- API/src/controllers/NotesControllers.js
+++ API/src/controllers/NotesControllers.js
@@ -56,12 +56,23 @@ class NotesControllers {
     }
 
     async all(req, res) {
-        const { user_id, title } = req.query;
+        const { user_id, title, tags } = req.query;
 
-        const notes = await knex('notes')
-        .where({user_id})
-        .whereLike('title', `%${title}%`)
-        .orderBy('title');
+        let notes;
+
+        if(tags){
+            const filterTags = tags.split(',').map(tag => tag.trim());
+
+            notes = await knex('tags').whereIn('name', filterTags);
+
+        } else {
+            notes = await knex('notes')
+            .where({user_id})
+            .whereLike('title', `%${title}%`)
+            .orderBy('title');
+        }
+
+
 
         return res.json(notes);
     }
