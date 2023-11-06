4f3485e34e071570ea51dfc5be8232d255ba9a3a
Vinicius Sousa <viniciusxsousa@outlook.com>
2023-11-05 21:20:03 -0300

    juntando tags com notas

--- API/src/controllers/NotesControllers.js
+++ API/src/controllers/NotesControllers.js
@@ -83,9 +83,17 @@ class NotesControllers {
             .orderBy('title');
         }
 
+        const userTags = await knex('tags').where({user_id});
+        const notesWithTags = notes.map(note => {
+            const noteTags = userTags.filter(tag => tag.note_id === note.id);
 
+            return {
+                ...note,
+                tags: noteTags
+            }
+        })        
 
-        return res.json(notes);
+        return res.json(notesWithTags);
     }
 }
 
