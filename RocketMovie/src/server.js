51f35a16017821de27d3d22a5f90beb2325d34be
Vinicius Sousa <viniciusxsousa@outlook.com>
2023-11-06 21:23:45 -0300

    configurando o tratamento de erro

--- RocketMovie/src/server.js
+++ RocketMovie/src/server.js
@@ -1,10 +1,30 @@
+require('express-async-errors');
+
 const express = require('express');
+
+const AppError = require('./utils/AppError');
 
 const app = express();
 const routes = require('./routes');
 
 app.use(express.json());
 app.use(routes);
+
+app.use( (error, request, response, next) => {
+    if(error instanceof AppError) {
+        return response.status(error.statusCode).json({
+            status: "error",
+            message: error.message
+        })
+    }
+
+    console.log(error);
+
+    return response.status(500).json({
+        status: 'error',
+        message: 'Erro interno.'
+    })
+})
 
 const PORT = 3333;
 
