96139ad810a8f5483cbe1ad8657a3a96212842a0
Vinicius Sousa <viniciusxsousa@outlook.com>
2023-11-08 13:00:11 -0300

    criando a função de cadastrar usuário e instalando a lib de criptografia

--- RocketMovie/src/controllers/UserControllers.js
+++ RocketMovie/src/controllers/UserControllers.js
@@ -1,9 +1,38 @@
+const AppError = require('../utils/AppError');
+const knex = require('../database/knex');
+const { hash } = require('bcryptjs');
+
 class UserControllers{
-    create(req, res){
-        console.log('Função de criar usuário.');
+    async create(req, res){
+
+        const { name, email, password } = req.body;
+
+        if(!name) {
+            throw new AppError('O nome é obrigatório.');
+        }
+
+        if(!password) {
+            throw new AppError('Uma senha deve ser informada.');
+        }
+
+        const userExist = await knex('users').where({email}).first();
+
+        if(userExist) {
+            throw new AppError('Usuário já existe.');
+        }
+
+        const hashPassword = await hash(password, 8);
+
+        await knex('users').insert({
+            name,
+            email,
+            password: hashPassword
+        })
 
         return res.json({
-            message: 'Cadastro realizado.'
+            name, 
+            email,
+            hashPassword
         });
     }
 }
