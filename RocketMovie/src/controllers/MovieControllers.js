71f0978d9009393aa0f0df4cb1e001da9f0a0fa0
Vinicius Sousa <viniciusxsousa@outlook.com>
2023-11-09 13:34:49 -0300

    associando o cadastro das tags junto com o cadastro de filme

--- RocketMovie/src/controllers/MovieControllers.js
+++ RocketMovie/src/controllers/MovieControllers.js
@@ -3,7 +3,7 @@ const knex = require('../database/knex');
 
 class MovieControllers{
     async create(req, res) {
-        const {title, description, rating} = req.body;
+        const {title, description, rating, tags} = req.body;
         const {user_id} = req.params;
 
         if(!title) {
@@ -18,12 +18,22 @@ class MovieControllers{
             throw new AppError('A nota é obrigatória.');
         }
 
-        await knex("movie_notes").insert({
+        const [movie_id] = await knex("movie_notes").insert({
             title,
             description,
             rating,
             user_id
         })
+
+        const tagsInsert = tags.map( name => {
+            return {
+                name,
+                movie_id,
+                user_id
+            }
+        })
+
+        await knex('movie_tags').insert(tagsInsert);
 
         return res.json({
             title,
