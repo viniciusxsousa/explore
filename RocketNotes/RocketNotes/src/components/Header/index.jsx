89355b24f16d1272acf1c995ad4af8307cf550bf
Vinicius Sousa <viniciusxsousa@outlook.com>
2023-11-22 14:18:07 -0300

    criando elementos de foto e nome do usuário no header

--- RocketNotes/RocketNotes/src/components/Header/index.jsx
+++ RocketNotes/RocketNotes/src/components/Header/index.jsx
@@ -1,7 +1,19 @@
-import { Conteiner } from "./styles";
+import { Conteiner, Profile} from "./styles";
 
 export function Header(){
     return (
-        <Conteiner></Conteiner>
+        <Conteiner>
+            <Profile>
+                <img 
+                    src="http://www.github.com/viniciusxsousa.png"
+                    alt="Foto de perfil do usuário"
+                />
+
+                <div>
+                    <span>Bem-vindo</span>
+                    <strong>Vinicius Sousa</strong>
+                </div>
+            </Profile>
+        </Conteiner>
     )
 }
--- RocketNotes/RocketNotes/src/components/Header/styles.js
+++ RocketNotes/RocketNotes/src/components/Header/styles.js
@@ -8,8 +8,36 @@ export const Conteiner = styled.header`
     border-bottom: 1px solid ${({theme}) => theme.COLORS.BACKGROUND_700};
     display: flex;
     justify-content: space-between;
+    align-items: center;
 
     padding: 0 80px;
+`
 
-    background-color: red;
+export const Profile = styled.div`
+    display: flex;
+    align-items: center;
+
+
+    > img {
+        width: 60px;
+        height: 60px;
+        border-radius: 50%;
+    }
+
+    > div {
+        display: flex;
+        flex-direction: column;
+        margin-left: 16px;
+        line-height: 24px;
+
+        span {
+            font-size: 14px;
+            color: ${({theme}) => theme.COLORS.GRAY_100};
+        }
+
+        strong {
+            font-size: 18px;
+            color: ${({theme}) => theme.COLORS.WHHITE};
+        }
+    }
 `
