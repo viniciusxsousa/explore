c655dc5153d302013e5c6143f66e46fd5991d850
Vinicius Sousa <viniciusxsousa@outlook.com>
2023-11-22 20:40:04 -0300

    criando o componente de section e o elemento de links em Details

--- RocketNotes/RocketNotes/src/pages/Details/index.jsx
+++ RocketNotes/RocketNotes/src/pages/Details/index.jsx
@@ -1,12 +1,22 @@
-import { Conteiner } from "./styles" 
+import { Conteiner, Links } from "./styles" 
 
 import { Button } from "../../components/Button"
 import { Header } from "../../components/Header"
+import { Section } from "../../components/Section"
 
 export function Details() {
     return (
         <Conteiner>
             <Header></Header>
+
+            <Section title='Links úteis'>
+                <Links>
+                    <li><a href="#">item 1</a></li>
+                    <li>item 2</li>
+                    <li>item 3</li>
+                </Links>
+            </Section>
+
             <Button title="Salvar"/>
         </Conteiner>
     )
