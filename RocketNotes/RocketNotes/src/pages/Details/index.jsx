75a422a03b58ac87945e95c33fda1a7d95cc756f
Vinicius Sousa <viniciusxsousa@outlook.com>
2023-11-22 13:41:38 -0300

    Componentizando os arquivos de details

--- RocketNotes/RocketNotes/src/pages/Details/index.jsx
+++ RocketNotes/RocketNotes/src/pages/Details/index.jsx
@@ -1,7 +1,7 @@
 import { Conteiner } from "./styles" 
 
-import { Button } from "../components/Button"
-import { Header } from "../components/Header"
+import { Button } from "../../components/Button"
+import { Header } from "../../components/Header"
 
 export function Details() {
     return (
