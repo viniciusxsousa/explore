961b869ee52a9a4b49e6d956a79b2f30fd52318d
Vinicius Sousa <viniciusxsousa@outlook.com>
2023-11-22 22:24:02 -0300

    finaliando a tela details

--- RocketNotes/RocketNotes/src/pages/Details/index.jsx
+++ RocketNotes/RocketNotes/src/pages/Details/index.jsx
@@ -1,4 +1,4 @@
-import { Conteiner, Links } from "./styles" 
+import { Conteiner, Links, Content } from "./styles" 
 
 import { Button } from "../../components/Button"
 import { Header } from "../../components/Header"
@@ -9,24 +9,35 @@ import { ButtonText } from "../../components/ButtonText"
 export function Details() {
     return (
         <Conteiner>
+
             <Header></Header>
 
-            <ButtonText title="Excluir Nota"/>
+            <main>
+                <Content>
 
-            <Section title='Links úteis'>
-                <Links>
-                    <li><a href="#">item 1</a></li>
-                    <li>item 2</li>
-                    <li>item 3</li>
-                </Links>
-            </Section>
+                    <ButtonText title="Excluir Nota"/>
 
-            <Section title='Marcadores'>
-                <Tag title='express'/>
-                <Tag title='node.js'/>
-            </Section>
+                    <h1>Introdução ao React</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita rerum illum fugiat vitae facere soluta vero architecto, nemo obcaecati quia hic repudiandae quae quas. Pariatur, repellendus. At, unde officia vel iste ab voluptatem voluptates quaerat voluptatum consequatur provident dolorum alias natus expedita. Officiis iure excepturi, corrupti molestias doloremque reprehenderit quasi!</p>
 
-            <Button title="Salvar"/>
+                    <Section title='Links úteis'>
+                        <Links>
+                            <li><a href="#">item 1</a></li>
+                            <li>item 2</li>
+                            <li>item 3</li>
+                        </Links>
+                    </Section>
+
+                    <Section title='Marcadores'>
+                        <Tag title='express'/>
+                        <Tag title='node.js'/>
+                    </Section>
+
+                    <Button title="Salvar"/>
+
+                </Content>
+            </main>
+
         </Conteiner>
     )
 }
--- RocketNotes/RocketNotes/src/pages/Details/styles.js
+++ RocketNotes/RocketNotes/src/pages/Details/styles.js
@@ -9,6 +9,12 @@ export const Conteiner = styled.div`
     grid-template-areas: 
     'header'
     'content';
+
+    > main {
+        grid-area: content;
+        overflow-y: scroll;
+        padding: 64px 0;
+    }
 `
 
 export const Links = styled.ul`
@@ -20,5 +26,29 @@ export const Links = styled.ul`
         a {
             color: ${({theme}) => theme.COLORS.WHITE}
         }
+    }
+`
+
+export const Content = styled.div`
+    max-width: 550px;
+    margin: 0 auto;
+
+    display: flex;
+    flex-direction: column;
+
+    > button:first-child {
+        align-self: end;
+    }
+
+    > h1 {
+        font-size: 36px;
+        padding-top: 60px;
+        font-weight: 500;
+    }
+
+    > p {
+        font-size: 16px;
+        text-align: justify;
+        margin-top: 16px;
     }
 `
