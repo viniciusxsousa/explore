d4d5cf5fe8a9d55b84067164bb7f3d1f62074509
Vinicius Sousa <viniciusxsousa@outlook.com>
2023-11-22 21:03:17 -0300

    criando componente tag

--- RocketNotes/RocketNotes/src/pages/Details/index.jsx
+++ RocketNotes/RocketNotes/src/pages/Details/index.jsx
@@ -3,6 +3,7 @@ import { Conteiner, Links } from "./styles"
 import { Button } from "../../components/Button"
 import { Header } from "../../components/Header"
 import { Section } from "../../components/Section"
+import { Tag } from '../../components/Tag'
 
 export function Details() {
     return (
@@ -15,6 +16,11 @@ export function Details() {
                     <li>item 2</li>
                     <li>item 3</li>
                 </Links>
+            </Section>
+
+            <Section title='Marcadores'>
+                <Tag title='express'/>
+                <Tag title='node.js'/>
             </Section>
 
             <Button title="Salvar"/>
