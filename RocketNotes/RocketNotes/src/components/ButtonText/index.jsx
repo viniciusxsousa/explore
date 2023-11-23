8f14742e8b247b2d8d821794b397f22ff1bc6259
Vinicius Sousa <viniciusxsousa@outlook.com>
2023-11-22 21:17:46 -0300

    criando o componente buttonTetx

---
+++ RocketNotes/RocketNotes/src/components/ButtonText/index.jsx
@@ -0,0 +1,12 @@
+import { Conteiner } from './styles'
+
+export function ButtonText({title, ...rest}) {
+    return (
+        <Conteiner
+            type='button'
+            {...rest}
+        >
+            {title}
+        </Conteiner>
+    )
+}
--- RocketNotes/RocketNotes/src/pages/Details/index.jsx
+++ RocketNotes/RocketNotes/src/pages/Details/index.jsx
@@ -4,11 +4,14 @@ import { Button } from "../../components/Button"
 import { Header } from "../../components/Header"
 import { Section } from "../../components/Section"
 import { Tag } from '../../components/Tag'
+import { ButtonText } from "../../components/ButtonText"
 
 export function Details() {
     return (
         <Conteiner>
             <Header></Header>
+
+            <ButtonText title="Excluir Nota"/>
 
             <Section title='Links úteis'>
                 <Links>
