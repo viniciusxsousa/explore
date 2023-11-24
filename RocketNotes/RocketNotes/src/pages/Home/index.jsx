4cb4e4e5186ae930b2b0689a8e1c631661633a44
Vinicius Sousa <viniciusxsousa@outlook.com>
2023-11-24 14:33:11 -0300

    finaliando o botão criar nota

--- RocketNotes/RocketNotes/src/pages/Home/index.jsx
+++ RocketNotes/RocketNotes/src/pages/Home/index.jsx
@@ -1,5 +1,6 @@
 import { Container,  Brand, Menu, Search, Content, NewNote} from './styles'
 
+import { FiPlus } from 'react-icons/fi'
 import { Header } from '../../components/Header'
 import {ButtonText} from '../../components/ButtonText'
 
@@ -25,7 +26,10 @@ export function Home(){
 
             <Content></Content>
 
-            <NewNote></NewNote>
+            <NewNote>
+                <FiPlus/>
+                Criar nota
+            </NewNote>
 
         </Container>
     )
