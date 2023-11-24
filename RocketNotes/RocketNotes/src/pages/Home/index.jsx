7f4bc6a5eae5da13c991822a1b98c6dbf6755951
Vinicius Sousa <viniciusxsousa@outlook.com>
2023-11-24 15:35:38 -0300

    criando componente de input

--- RocketNotes/RocketNotes/src/pages/Home/index.jsx
+++ RocketNotes/RocketNotes/src/pages/Home/index.jsx
@@ -1,8 +1,9 @@
 import { Container,  Brand, Menu, Search, Content, NewNote} from './styles'
 
-import { FiPlus } from 'react-icons/fi'
-import { Header } from '../../components/Header'
 import {ButtonText} from '../../components/ButtonText'
+import { Header } from '../../components/Header'
+import { FiPlus, FiSearch} from 'react-icons/fi'
+import { Input } from '../../components/Input'
 
 export function Home(){
     return (
@@ -22,7 +23,9 @@ export function Home(){
 
             </Menu>
 
-            <Search></Search>
+            <Search>
+                <Input icon={FiSearch} placeholder='Pesquise pelo título'/>
+            </Search>
 
             <Content></Content>
 
