c6b0a2ac9caf859b23e60191531638e3fd9d22f1
Vinicius Sousa <viniciusxsousa@outlook.com>
2023-11-25 14:49:42 -0300

    criando a tela de signIn

--- RocketNotes/RocketNotes/src/pages/SignIn/index.jsx
+++ RocketNotes/RocketNotes/src/pages/SignIn/index.jsx
@@ -1,9 +1,36 @@
-import { Container } from "./styles";
+import { Container, Form } from "./styles";
+
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
+
+import { FiMail, FiLock } from 'react-icons/fi'
 
 export function SignIn(){
     return(
         <Container>
-            
+            <Form>
+
+                <h1>Rocket Notes</h1>
+                <p>Aplicação para salvar e gerenciar seus links úteis.</p>
+
+                <h2>Faça seu login</h2>
+
+                <Input 
+                    icon={FiMail}
+                    placeholder="E-mail"
+                    type='email'
+                />
+
+                <Input
+                    icon={FiLock}
+                    placeholder="Senha"
+                    type='password'
+                />
+                
+                <Button title='Entrar'/>
+
+                <a href="#">Criar conta</a>
+            </Form>
         </Container>
     )
 }
