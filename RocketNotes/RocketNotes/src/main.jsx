4689370f31af754353641909807808793cec7575
Vinicius Sousa <viniciusxsousa@outlook.com>
2023-11-27 10:41:55 -0300

    criando a interface new

--- RocketNotes/RocketNotes/src/main.jsx
+++ RocketNotes/RocketNotes/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Profile } from './pages/Profile'
+import { New } from './pages/New'
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
 import GlobalStyle from './styles/global'
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle/>
-      <Profile></Profile>
+      <New></New>
     </ThemeProvider>
   </React.StrictMode>,
 )
