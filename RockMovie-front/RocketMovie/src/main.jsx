2e975de826d55a14d2e8e6ba74ba82262bfa4d6a
Vinicius Sousa <viniciusxsousa@outlook.com>
2023-11-29 11:30:18 -0300

    organização na estrutura de página

--- RockMovie-front/RocketMovie/src/main.jsx
+++ RockMovie-front/RocketMovie/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App.jsx'
+import { Home } from './pages/Home/'
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
 import GlobalStyles from './styles/global'
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <App />
+      <Home />
     </ThemeProvider>
   </React.StrictMode>,
 )
--- RockMovie-front/RocketMovie/src/pages/Home/index.jsx
+++ RockMovie-front/RocketMovie/src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-export function App(){
+export function Home(){
   return(
     <h1>Olá, mundo!</h1>
   )
