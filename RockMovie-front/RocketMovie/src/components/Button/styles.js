cccf8d09dc5544edec7bf308d1c055431fd3ed5a
Vinicius Sousa <viniciusxsousa@outlook.com>
2023-11-29 13:59:59 -0300

    criando o componente button

---
+++ RockMovie-front/RocketMovie/src/components/Button/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const Container = styled.button`
+    width: 100%;
+    border-radius: 8px;
+    padding: 32px;
+    background-color: ${({theme}) => theme.COLORS.PINK};
+`
