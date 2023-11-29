0209ec7cad152a6231318820b47188da279a9ed0
Vinicius Sousa <viniciusxsousa@outlook.com>
2023-11-29 10:49:07 -0300

    definindo e configurando os arquivos de estilo

---
+++ RockMovie-front/RocketMovie/src/styles/theme.js
@@ -0,0 +1,20 @@
+export default {
+    COLORS: {
+        BACKGROUND_100: '#262529',
+        BACKGROUND_200: 'rgba(255, 133, 155, 0.05)',
+        BACKGROUND_300: '#1C1B1E',
+        
+        PINK: '#FF859B',
+
+        WHITE_100: '#E5E5E5',
+        WHITE_200: '#F4EDE8',
+        WHITE_300: '#FFF',
+
+        GRAY_100: '#999591',
+        GRAY_200: '#948F99',
+
+
+        BLACK_100: '#1C1B1E',
+        BLACK__200: '#0D0C0F',
+    }
+}
