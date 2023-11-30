f8e007bfd4a6204039d41b13a6327ea619e60f8c
Vinicius Sousa <viniciusxsousa@outlook.com>
2023-11-30 14:51:37 -0300

    criando a tela details

--- RockMovie-front/RocketMovie/src/main.jsx
+++ RockMovie-front/RocketMovie/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { New } from './pages/New/'
+import { Details } from './pages/Details/'
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
 import GlobalStyles from './styles/global'
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <New />
+      <Details />
     </ThemeProvider>
   </React.StrictMode>
 )
