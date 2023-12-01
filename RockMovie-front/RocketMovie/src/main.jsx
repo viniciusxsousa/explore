ff38025eceeea8e4c4043f4e696de2f4753fb2db
Vinicius Sousa <viniciusxsousa@outlook.com>
2023-12-01 14:19:39 -0300

    confiruando as rotas

--- RockMovie-front/RocketMovie/src/main.jsx
+++ RockMovie-front/RocketMovie/src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { SingUp } from './pages/SingUp/'
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
 import GlobalStyles from './styles/global'
+import { Routes } from  './routes'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <SingUp />
+      <Routes/>
     </ThemeProvider>
   </React.StrictMode>
 )
