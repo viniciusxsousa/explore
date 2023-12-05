d9c283eba72102d3dae01faa82d6e56a6e13982c
Vinicius Sousa <viniciusxsousa@outlook.com>
2023-12-05 16:20:14 -0300

    criando as rotas e o controller de session

---
+++ API/src/controllers/SessionsControllers.js
@@ -0,0 +1,9 @@
+class SessionsControllers {
+    async create(req, res) {
+        const {email, password} = req.body;
+        
+        return res.json({email, password});
+    }
+}
+
+module.exports = SessionsControllers
--- API/src/routes/index.js
+++ API/src/routes/index.js
@@ -5,10 +5,12 @@ const routes = Router();
 const userRoutes = require('./user.routes');
 const noteRoutes = require('./note.routes');
 const tagsRoutes = require('./tags.routes');
+const sessionRoute = require('./sessions.routes');
 
 
 routes.use("/user", userRoutes);
 routes.use("/note", noteRoutes);
 routes.use("/tags", tagsRoutes);
+routes.use("/session", sessionRoute)
 
 module.exports = routes
