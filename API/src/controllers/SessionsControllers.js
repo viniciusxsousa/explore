160a3f70fbc36c9b62a273ee1e2341459a1b7809
Vinicius Sousa <viniciusxsousa@outlook.com>
2023-12-05 16:34:39 -0300

    validando se o e-mail de usuário existe

--- API/src/controllers/SessionsControllers.js
+++ API/src/controllers/SessionsControllers.js
@@ -1,8 +1,17 @@
+const knex = require('../database/knex');
+const AppError = require ('../utils/AppError');
+
 class SessionsControllers {
     async create(req, res) {
         const {email, password} = req.body;
+
+        const user = await knex('users').where({email}).first();
+
+        if(!user) {
+            throw new AppError('E-mail e/ou senha incorreta', 401);
+        }  
         
-        return res.json({email, password});
+        return res.json({user});
     }
 }
 
