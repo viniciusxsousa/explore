6ed53862499b986cc5a828624f31744a5d280546
Vinicius Sousa <viniciusxsousa@outlook.com>
2023-12-05 16:45:43 -0300

    verificando se a senha do usuário é válida

--- API/src/controllers/SessionsControllers.js
+++ API/src/controllers/SessionsControllers.js
@@ -1,5 +1,6 @@
 const knex = require('../database/knex');
 const AppError = require ('../utils/AppError');
+const { compare } = require('bcryptjs');
 
 class SessionsControllers {
     async create(req, res) {
@@ -9,7 +10,13 @@ class SessionsControllers {
 
         if(!user) {
             throw new AppError('E-mail e/ou senha incorreta', 401);
-        }  
+        } 
+        
+        const passwordMatched = await compare(password, user.password);
+
+        if(!passwordMatched) {
+            throw new AppError("E-mail e/ou senha incorreta", 401);
+        }
         
         return res.json({user});
     }
