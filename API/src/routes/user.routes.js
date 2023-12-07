0a6af1aec7402c0a00e7a1ebce3d97c91fb8cb51
Vinicius Sousa <viniciusxsousa@outlook.com>
2023-12-07 11:06:22 -0300

    retirando código não utilizado

--- API/src/routes/user.routes.js
+++ API/src/routes/user.routes.js
@@ -6,14 +6,7 @@ const UserControllers = require("../controllers/UserControllers");
 
 const userControllers = new UserControllers();
 
-function myMiddleware(req, res, next) {
-    console.log('exibindo os dados da requisição:');
-    console.log(req.body);
-    next();
-}
-
-
-userRoutes.post('/', myMiddleware, userControllers.create);
+userRoutes.post('/', userControllers.create);
 userRoutes.put('/:id', userControllers.update);
 
 module.exports = userRoutes;
