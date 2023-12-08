82dd4fafb5403a140172f5af84ef0db76bb61a1b
Vinicius Sousa <viniciusxsousa@outlook.com>
2023-12-08 11:26:53 -0300

    salvando a imagem no banco

--- API/src/routes/user.routes.js
+++ API/src/routes/user.routes.js
@@ -6,16 +6,16 @@ const userRoutes = Router();
 const upload = multer(uploadConfig.MULTER);
 
 const UserControllers = require("../controllers/UserControllers");
+const UserAvatarControllers = require('../controllers/UserAvatarControllers');
 
 const ensureAuthenticated = require('../middleware/ensureAuthenticated')
 
 const userControllers = new UserControllers();
+const userAvatarControllers = new UserAvatarControllers();
 
 userRoutes.post('/', userControllers.create);
 userRoutes.put('/', ensureAuthenticated, userControllers.update);
-userRoutes.patch('/avatar', ensureAuthenticated, upload.single('avatar'), (req, res) => {
-    console.log(req.file.filename);
-    res.json();
-})
+
+userRoutes.patch('/avatar', ensureAuthenticated, upload.single('avatar'), userAvatarControllers.update);
 
 module.exports = userRoutes;
