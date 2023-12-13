68f3bbd577bc6602febb0ec1ee6e75975f50055c
Vinicius Sousa <viniciusxsousa@outlook.com>
2023-12-13 13:31:21 -0300

    ajustando o header das requisições

--- RocketNotes/RocketNotes/src/hook/auth.jsx
+++ RocketNotes/RocketNotes/src/hook/auth.jsx
@@ -18,7 +18,7 @@ function AuthProvider({ children }) {
             localStorage.setItem("@rocketnotes:user", JSON.stringify(user));
             localStorage.setItem("@rocketnotes:token", token)
 
-            api.defaults.headers.authorization = `Bearer ${token}`;
+            api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 
             setData({token, user});
 
@@ -43,7 +43,7 @@ function AuthProvider({ children }) {
         const user = localStorage.getItem("@rocketnotes:user");
 
         if(token && user) {
-            api.defaults.headers.authorization = `Bearer ${token}`;
+            api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 
             setData({
                 token,
