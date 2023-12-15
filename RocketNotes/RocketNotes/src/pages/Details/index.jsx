a12ea857aa528dedeaf54cad76ce6c045ea75750
Vinicius Sousa <viniciusxsousa@outlook.com>
2023-12-15 20:57:06 -0300

    carregando detalhes da nota

--- RocketNotes/RocketNotes/src/pages/Details/index.jsx
+++ RocketNotes/RocketNotes/src/pages/Details/index.jsx
@@ -1,4 +1,10 @@
+import { useEffect, useState } from "react"
+import { useParams, useNavigate } from "react-router-dom"
+
+import { api } from "../../services"
+
 import { Conteiner, Links, Content } from "./styles" 
+
 
 import { Button } from "../../components/Button"
 import { Header } from "../../components/Header"
@@ -7,36 +13,66 @@ import { Tag } from '../../components/Tag'
 import { ButtonText } from "../../components/ButtonText"
 
 export function Details() {
+    const [data, setData] = useState(null);
+
+    const params = useParams();
+    const navigator = useNavigate();
+
+    function handleBack() {
+        navigator('/');
+    }
+
+    useEffect(() => {
+        async function loadNote() {
+            const response = await api.get(`/note/${params.id}`);
+            setData(response.data);
+        }
+
+        loadNote();
+    }, [])
+
     return (
         <Conteiner>
 
             <Header></Header>
 
-            <main>
-                <Content>
+            {
+                data && 
+                <main>
+                    <Content>
 
-                    <ButtonText title="Excluir Nota"/>
+                        <ButtonText title="Excluir Nota"/>
 
-                    <h1>Introdução ao React</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita rerum illum fugiat vitae facere soluta vero architecto, nemo obcaecati quia hic repudiandae quae quas. Pariatur, repellendus. At, unde officia vel iste ab voluptatem voluptates quaerat voluptatum consequatur provident dolorum alias natus expedita. Officiis iure excepturi, corrupti molestias doloremque reprehenderit quasi!</p>
+                        <h1>{data.title}</h1>
+                        <p>{data.description}</p>
 
-                    <Section title='Links úteis'>
-                        <Links>
-                            <li><a href="#">item 1</a></li>
-                            <li>item 2</li>
-                            <li>item 3</li>
-                        </Links>
-                    </Section>
+                        {
+                            data.links && 
+                            <Section title='Links úteis'>
+                                <Links>
+                                    {data.links.map( (link) => (
+                                        <li key={String(link.id)}>
+                                            <a href={link.url} target='_black'>{link.url}</a>
+                                        </li>
+                                    ))}
+                                </Links>
+                            </Section>
+                        }
 
-                    <Section title='Marcadores'>
-                        <Tag title='express'/>
-                        <Tag title='node.js'/>
-                    </Section>
+                        {
+                            data.tags && 
+                            <Section title='Marcadores'>
+                                {data.tags.map((tag) => (
+                                    <Tag key={tag.id} title={tag.name}/>
+                                ))}
+                            </Section>
+                        }
 
-                    <Button title="Salvar"/>
+                        <Button title="Voltar" onClick={handleBack}/>
 
-                </Content>
-            </main>
+                    </Content>
+                </main>
+            }
 
         </Conteiner>
     )
