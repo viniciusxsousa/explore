66c50cc85a1a0df11264415853e6c4b664be301d
Vinicius Sousa <viniciusxsousa@outlook.com>
2023-12-18 12:20:14 -0300

    validando se a senha informada é válida

--- RocketMovie/src/controllers/SessionsController.js
+++ RocketMovie/src/controllers/SessionsController.js
@@ -1,5 +1,6 @@
 const knex = require('../database/knex');
 const AppError = require('../utils/AppError');
+const { compare } = require('bcryptjs'); 
 
 class SessionsController {
     async create(req, res) {
@@ -10,6 +11,12 @@ class SessionsController {
         if(!user) {
             throw new AppError("E-mail e/ou senha incorreta", 401);
         }
+
+        const passwordMacth = await compare(password, user.password);
+
+        if(!passwordMacth) {
+            throw new AppError("E-mail e/ou senha incorreta", 401);
+        } 
  
         return res.json(user);
     }
