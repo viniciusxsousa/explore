693aeb540e32baebd00a86989ef77349d728826c
Vinicius Sousa <viniciusxsousa@outlook.com>
2023-12-19 22:08:12 -0300

    carregando imagens

--- RocketMovie/src/routes/user.routes.js
+++ RocketMovie/src/routes/user.routes.js
@@ -1,6 +1,9 @@
 const {Router} = require("express");
+const multer = require('multer');
+const uploadConfig = require('../configs/upload');
 
 const userRouter = Router();
+const upload = multer(uploadConfig.MULTER);
 
 const UserControllers = require('../controllers/UserControllers');
 
@@ -10,5 +13,9 @@ const userControllers = new UserControllers();
 
 userRouter.post('/', userControllers.create);
 userRouter.put('/', ensureAuthenticated, userControllers.update);
+userRouter.patch('/avatar', ensureAuthenticated, upload.single("avatar"), (req, res) => {
+    console.log(req.file.filename);
+    res.json();
+})
 
 module.exports = userRouter
