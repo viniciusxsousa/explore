1badd237eb63b2ab2126b9b2ef7586494470648a
Vinicius Sousa <viniciusxsousa@outlook.com>
2023-12-20 09:58:13 -0300

    salvando no banco de dados

--- RocketMovie/src/routes/user.routes.js
+++ RocketMovie/src/routes/user.routes.js
@@ -6,16 +6,15 @@ const userRouter = Router();
 const upload = multer(uploadConfig.MULTER);
 
 const UserControllers = require('../controllers/UserControllers');
+const UserAvatarControllers = require('../controllers/UserAvatarControllers');
 
 const ensureAuthenticated = require('../middleware/ensureAuthenticated');
 
 const userControllers = new UserControllers();
+const userAvatarControllers = new UserAvatarControllers();
 
 userRouter.post('/', userControllers.create);
 userRouter.put('/', ensureAuthenticated, userControllers.update);
-userRouter.patch('/avatar', ensureAuthenticated, upload.single("avatar"), (req, res) => {
-    console.log(req.file.filename);
-    res.json();
-})
+userRouter.patch('/avatar', ensureAuthenticated, upload.single("avatar"), userAvatarControllers.update);
 
 module.exports = userRouter
