9a7472327aa8d633e078800e7f54632057be81cf
Vinicius Sousa <viniciusxsousa@outlook.com>
2023-12-28 18:38:02 -0300

    carregando detalhes do filme

--- RockMovie-front/RocketMovie/src/pages/Details/index.jsx
+++ RockMovie-front/RocketMovie/src/pages/Details/index.jsx
@@ -1,3 +1,8 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+import { api } from "../../services/api";
+
 import { Container, Content } from "./styles";
 
 import { FaStar, FaRegStar, FaClock } from 'react-icons/fa'
@@ -7,6 +12,33 @@ import { ButtonTxt } from '../../components/ButtonTxt'
 import { Tag } from '../../components/Tag'
 
 export function Details(){
+    const [data, setData] = useState('');
+
+    const params = useParams();
+
+    useEffect(() => {
+        async function fetchMovie() {
+            const response = await api.get(`/movie/${params.id}`);
+            console.log(response.data);
+            setData(response.data);
+        }
+
+        fetchMovie();
+    }, [])
+
+    const star = data.rating;
+
+    let startCompleted = [];
+    let startNotCompleted = [];
+
+    for(let i = 0; i < star; i++) {
+        startCompleted.push(i);
+    }
+
+    for(let i = (5 - star); i > 0; i--) {
+        startNotCompleted.push(i);
+    }
+
     return(
         <Container>
             <Header/>
@@ -16,25 +48,38 @@ export function Details(){
                     <ButtonTxt title='voltar' icon to='/'/>
 
                     <div className="title">
-                        <h2>Interestellar</h2>
-                        <FaStar/>
-                        <FaStar/>
-                        <FaStar/>
-                        <FaStar/>
-                        <FaRegStar/>
+                        <h2>{data.title}</h2>
+
+                        { 
+                          startCompleted.map((item) => (
+                              <FaStar key={item}/>
+                          ))
+                        }
+
+                        {
+                            startNotCompleted.map((item) => (
+                                <FaRegStar key={item}/>
+                            ))
+                        }
+
                     </div>
 
                     <div className="time">
                         <FaClock /> 
-                        <p>30/11/2023</p>
+                        <p>{data.updated_at}</p>
                     </div>
 
-                    <Tag title='Drama' brown/>
-                    <Tag title='Aventura' brown/>
+                    { data.tags && 
+                        data.tags.map((tag) => (
+                            <Tag 
+                                key={tag.id}
+                                title={tag.name} 
+                                brown
+                            />
+                        ))
+                    }
 
-                    <p>Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se comunicar com ela. Pai e filha descobrem que o "fantasma" é uma inteligência desconhecida que está enviando mensagens codificadas através de radiação gravitacional, deixando coordenadas em binário que os levam até uma instalação secreta da NASA liderada pelo professor John Brand. O cientista revela que um buraco de minhoca foi aberto perto de Saturno e que ele leva a planetas que podem oferecer condições de sobrevivência para a espécie humana. As "missões Lázaro" enviadas anos antes identificaram três planetas potencialmente habitáveis orbitando o buraco negro Gargântua: Miller, Edmunds e Mann – nomeados em homenagem aos astronautas que os pesquisaram. Brand recruta Cooper para pilotar a nave espacial Endurance e recuperar os dados dos astronautas; se um dos planetas se mostrar habitável, a humanidade irá seguir para ele na instalação da NASA, que é na realidade uma enorme estação espacial. A partida de Cooper devasta Murphy.</p> 
-                    
-                    <p>Além de Cooper, a tripulação da Endurance é formada pela bióloga Amelia, filha de Brand; o cientista Romilly, o físico planetário Doyle, além dos robôs TARS e CASE. Eles entram no buraco de minhoca e se dirigem a Miller, porém descobrem que o planeta possui enorme dilatação gravitacional temporal por estar tão perto de Gargântua: cada hora na superfície equivale a sete anos na Terra. Eles entram em Miller e descobrem que é inóspito já que é coberto por um oceano raso e agitado por ondas enormes. Uma onda atinge a tripulação enquanto Amelia tenta recuperar os dados de Miller, matando Doyle e atrasando a partida. Ao voltarem para a Endurance, Cooper e Amelia descobrem que 23 anos se passaram.</p>
+                    <p>{data.description}</p> 
 
                 </Content>
             </main>
