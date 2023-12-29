cc0d05ca1c8fe4d8aa8cc82504903e8d925968c5
Vinicius Sousa <viniciusxsousa@outlook.com>
2023-12-29 10:06:41 -0300

    tratando de rotas indisponíveis

--- RockMovie-front/RocketMovie/src/routes/app.routes.jsx
+++ RockMovie-front/RocketMovie/src/routes/app.routes.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 import { Home } from '../pages/Home'
 import { Profile } from '../pages/Profile'
@@ -12,6 +12,8 @@ export function AppRoutes(){
             <Route path='/profile' element={<Profile/>}/>
             <Route path='/new' element={<New/>}/>
             <Route path='/details/:id' element={<Details/>}/>
+
+            <Route path='*' element={<Navigate to="/"/>}/>
         </Routes>
     )
 }
