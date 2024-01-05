f1e2f8407556a1e0265c02666c8b3bfeb55cf6b1
Vinicius Sousa <viniciusxsousa@outlook.com>
2024-01-05 15:04:37 -0300

    criando um repositoryInMemory

--- API/src/services/userCreateService.spec.js
+++ API/src/services/userCreateService.spec.js
@@ -1,13 +1,15 @@
 const UserCreateService = require('./UserCreateService');
+const UserRepositoryInMemory = require('../repositories/UserRepositoryInMemory.js');
 
-it("user must be registered", () => {
+it("user must be registered", async () => {
     const user = {
         name: "User Test",
         email: "test@mail.com",
         password: "123" 
     };
 
-    const userCreateService = new UserCreateService();
+    const userRepositoryInMemory = new UserRepositoryInMemory();
+    const userCreateService = new UserCreateService(userRepositoryInMemory);
     const userCreated = await userCreateService.execute(user);
 
     expect(userCreated).toHaveProperty("id");
