d81ec1df915600eff723748fd9f7e8dc7e3531b0
Vinicius Sousa <viniciusxsousa@outlook.com>
2024-01-04 21:04:37 -0300

    utilizando o userRepository

--- API/src/controllers/UserControllers.js
+++ API/src/controllers/UserControllers.js
@@ -2,12 +2,15 @@ const AppError = require('../utils/AppError');
 const sqliteConnection = require('../database/sqlite');
 const { hash, compare } = require('bcryptjs');
 
+const UserRepository = require('../repositories/UserRepository');
+
 class UserControllers {
     async create(req, res) {
         const { name, email, password } = req.body
 
-        const database = await sqliteConnection();
-        const checkUserExist = await database.get('SELECT * FROM users WHERE email = (?)', [email]);
+        const userRepository = new UserRepository();
+
+        const checkUserExist = await userRepository.findByEmail(email);
 
         if(checkUserExist) {
             throw new AppError('O usuário já existe!');
@@ -15,10 +18,7 @@ class UserControllers {
 
         const hashedPassword = await hash(password, 8);
 
-        await database.run(
-            'INSERT INTO users (name, email, password) VALUES (?, ?, ?)',
-            [name, email, hashedPassword]
-        );
+        await userRepository.create({name, email, password: hashedPassword});
 
         return res.status(201).json();
 
