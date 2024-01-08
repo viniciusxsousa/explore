0ce70299942a4cc1d86e86720befe44bc37c7a05
Vinicius Sousa <viniciusxsousa@outlook.com>
2024-01-08 16:25:36 -0300

    criando o teste para cadastro de usuário com e-mail já existente

--- API/src/services/userCreateService.spec.js
+++ API/src/services/userCreateService.spec.js
@@ -1,5 +1,6 @@
 const UserCreateService = require('./UserCreateService');
 const UserRepositoryInMemory = require('../repositories/UserRepositoryInMemory.js');
+const AppError = require('../utils/AppError');
 
 describe('User Tests', () => {
     it("user must be registered", async () => {
@@ -16,9 +17,26 @@ describe('User Tests', () => {
         expect(userCreated).toHaveProperty("id");
     })
 
-    it("Another test", () => {
-        expect(1).toBe(2);
-    });
+    it('User must not register with existing email', async () => {
+        const user1 = {
+            name: 'user 01',
+            email: 'user@mail.com',
+            password: '123'
+        };
+
+        const user2 = {
+            name: 'user 02',
+            email: 'user@mail.com',
+            password: '456'
+        };
+
+        const userRepositoryInMemory = new UserRepositoryInMemory();
+        const userCreateService = new UserCreateService(userRepositoryInMemory);
+
+        await userCreateService.execute(user1);
+        await expect(userCreateService.execute(user2)).rejects.toEqual(new AppError("O usuário já existe!"));
+
+    })
 
 })
 
