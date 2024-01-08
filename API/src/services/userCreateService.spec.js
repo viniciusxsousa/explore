355fbd3d185d23140798589584a969bb58ad5517
Vinicius Sousa <viniciusxsousa@outlook.com>
2024-01-08 15:46:40 -0300

    agrupando os test com describe

--- API/src/services/userCreateService.spec.js
+++ API/src/services/userCreateService.spec.js
@@ -1,16 +1,24 @@
 const UserCreateService = require('./UserCreateService');
 const UserRepositoryInMemory = require('../repositories/UserRepositoryInMemory.js');
 
-it("user must be registered", async () => {
-    const user = {
-        name: "User Test",
-        email: "test@mail.com",
-        password: "123" 
-    };
+describe('User Tests', () => {
+    it("user must be registered", async () => {
+        const user = {
+            name: "User Test",
+            email: "test@mail.com",
+            password: "123" 
+        };
+    
+        const userRepositoryInMemory = new UserRepositoryInMemory();
+        const userCreateService = new UserCreateService(userRepositoryInMemory);
+        const userCreated = await userCreateService.execute(user);
+    
+        expect(userCreated).toHaveProperty("id");
+    })
 
-    const userRepositoryInMemory = new UserRepositoryInMemory();
-    const userCreateService = new UserCreateService(userRepositoryInMemory);
-    const userCreated = await userCreateService.execute(user);
+    it("Another test", () => {
+        expect(1).toBe(2);
+    });
 
-    expect(userCreated).toHaveProperty("id");
 })
+
